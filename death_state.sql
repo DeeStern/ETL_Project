02cb474ea36bc71896e16652975a6d1dcc617a4e
Devin Stern <writedevin@gmail.com>
2019-05-18 11:02:30 -0500

    Final write-up

---
+++ death_state.sql
@@ -0,0 +1,16 @@
+CREATE  DATABASE Death_Candy;
+
+USE Death_Candy;
+
+CREATE TABLE death_state (
+_Year INT,
+Cause_Name TEXT,
+State TEXT,
+Deaths INT
+);
+
+DROP TABLE death_state;
+
+SELECT * FROM death_state;
+
+DROP TABLE death_candy;
